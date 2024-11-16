b690f66eb7fef080816cd602879e524f73124a02
Gouravmodi25 <gouravmodi39@gmail.com>
2024-11-16 21:55:18 +0530

    complete login page ui

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,100 @@
+import { useState } from "react";
+
 const Login = () => {
-  return <div></div>;
+  const [state, setState] = useState("Sign up");
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <form
+      onSubmit={(e) => onSubmitHandler(e)}
+      className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto border  items-start p-8 min-w-[340px] sm:min-w-96 rounded-xl text-zinc-600 text-sm shadow-lg">
+        <p className="text-2xl font-semibold">
+          {state == "Sign up" ? "Create account" : "Login"}
+        </p>
+        <p>
+          Please {state == "Sign up" ? "sign up" : "login"} to book appointment
+        </p>
+        {state == "Sign up" && (
+          <div className="w-full">
+            <p>Full Name</p>
+            <input
+              className="border border-zinc-300 rounded w-full p-2 mt-1"
+              type="text"
+              onChange={(e) => setName(e.target.name)}
+              value={name}
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+        )}
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div className="relative w-full">
+          <p>Password</p>
+          <input
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+            type={showPassword ? "text" : "password"}
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            placeholder="Enter your password"
+            maxLength="8"
+            required
+          />
+          {password && (
+            <button
+              onClick={togglePasswordVisibility}
+              type="button"
+              className="absolute  inset-y-[43px] right-3 flex items-center text-gray-500 hover:text-blue-500">
+              {showPassword ? "🙈" : "👁️"}
+            </button>
+          )}
+        </div>
+        <button className="bg-primary text-base rounded-lg w-full p-2 text-white outline-none">
+          {state == "Sign up" ? "Create Account" : "Login"}
+        </button>
+        {state == "Sign up" ? (
+          <p>
+            Already have an account?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className="cursor-pointer text-primary">
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create an new account?{" "}
+            <span
+              onClick={() => setState("Sign up")}
+              className="text-primary cursor-pointer">
+              Click here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
 };
 
 export default Login;
