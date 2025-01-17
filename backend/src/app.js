04bd3ef19c4c52b83a1717917b04c3f2112fda89
Gouravmodi25 <gouravmodi39@gmail.com>
2025-01-18 03:28:08 +0530

    fix: update allowed origins in backend configuration to include production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5174",
-  "http://localhost:5173",
+  "https://prescrpito-gourav.netlify.app/doctors",
   "http://localhost:5175",
 ]; // Add your allowed origins here
 
